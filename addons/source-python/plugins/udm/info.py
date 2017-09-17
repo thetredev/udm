859a93cec0b9cf40e2e3ef812b6aa8b16b41d94a
BackRaw <backraw@gmx.de>
2017-09-17 18:08:39 +0200

    Add plugin info
    Signed-off-by: BackRaw <backraw@gmx.de>

---
+++ addons/source-python/plugins/udm/info.py
@@ -0,0 +1,16 @@
+# ../udm/info.py
+
+"""Provides/stores information about the plugin."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Plugins
+from plugins.info import PluginInfo
+
+
+# =============================================================================
+# >> PLUGIN INFO
+# =============================================================================
+info = PluginInfo('udm', verbose_name='UDM', author='BackRaw', version='1.0')
