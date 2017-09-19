b623c48b695bc3ab2e2fbd129f365a66c94a6184
BackRaw <backraw@gmx.de>
2017-09-19 07:21:28 +0200

    udm.py: Implement respawning by implementing an on_player_death() listener
    Signed-off-by: BackRaw <backraw@gmx.de>

--- addons/source-python/plugins/udm/udm.py
+++ addons/source-python/plugins/udm/udm.py
@@ -16,6 +16,7 @@ from listeners.tick import Delay
 # Script Imports
 #   Config
 from udm.config import cvar_equip_delay
+from udm.config import cvar_respawn_delay
 from udm.config import cvar_saycommand
 #   Menus
 from udm.menus import secondary_menu
@@ -35,6 +36,20 @@ def on_player_spawn(event):
     # Prepare the player if they're alive and on a team
     if player.team > 1 and not player.dead:
         Delay(abs(cvar_equip_delay.get_float()), player.prepare)
+
+
+@Event('player_death')
+def on_player_death(event):
+    """Remove all weapons the player owns and respawn the player."""
+    # Get a udm.players.PlayerEntity instance for the victim's userid
+    victim = PlayerEntity.from_userid(event.get_int('userid'))
+
+    # Remove all the weapons the victim currently owns
+    for weapon in victim.weapons():
+        weapon.remove()
+
+    # Respawn the victim using the cvar 'udm_respawn_delay'
+    Delay(abs(cvar_respawn_delay.get_float()), victim.spawn, (True,))
 
 
 # =============================================================================
