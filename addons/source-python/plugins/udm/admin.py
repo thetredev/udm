2dcd37c8ae2595a9e71062cb714e27935e6c766f
BackRaw <backraw@gmx.de>
2017-11-05 02:24:30 +0100

    admin.py: Create the Admin menu using a class
    This way, we can pass `self` to submenus.
    
    This change also includes calling players.py/PlayerEntity.equip() using
    the parameter `admin=True` which will restore the player's equipment to
    normal.
    
    Signed-off-by: BackRaw <backraw@gmx.de>

--- addons/source-python/plugins/udm/admin.py
+++ addons/source-python/plugins/udm/admin.py
@@ -7,21 +7,39 @@
 # =============================================================================
 # Source.Python Imports
 #   Menus
-from menus.radio import PagedRadioMenu
 from menus.radio import PagedRadioOption
 
 # Script Imports
+#   Menus
+from udm.menus import CloseButtonPagedMenu
+#   Players
+from udm.players import PlayerEntity
 #   Spawn Points
-from udm.spawnpoints.menus import spawnpoint_manager_menu
+from udm.spawnpoints.menus import SpawnPointManagerMenu
+
+
+# =============================================================================
+# >> PRIVATE CLASSES
+# =============================================================================
+class _AdminMenu(CloseButtonPagedMenu):
+    """Class used to provide a base menu for each of the following submenus:
+
+        - Spawn Point Manager
+    """
+
+    def __init__(self):
+        """Object initialization"""
+        super().__init__(
+            lambda player_index: PlayerEntity(player_index).equip(admin=True),
+            data=[
+                PagedRadioOption('SpawnPoint Manager', SpawnPointManagerMenu(self)),
+            ],
+            select_callback=lambda menu, player_index, option: option.value.send(player_index)
+        )
 
 
 # =============================================================================
 # >> PUBLIC GLOBAL VARIABLES
 # =============================================================================
-# Create the admin menu; on select: send the specified menu to the player
-admin_menu = PagedRadioMenu(
-    [
-        PagedRadioOption('SpawnPoint Manager', spawnpoint_manager_menu)
-    ],
-    select_callback=lambda menu, player_index, option: option.value.send(player_index)
-)
+# Create the Admin menu
+admin_menu = _AdminMenu()
