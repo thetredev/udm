97311b44db5ba55a07858b7b3b7fd29471b4f43d
BackRaw <backraw@gmx.de>
2017-11-05 02:16:50 +0100

    Add menus.py
    This file provides a class to add a 'close callback' to the
    PagedRadioMenu, which is always called if the player selects
    the BUTTON_CLOSE index.
    
    Signed-off-by: BackRaw <backraw@gmx.de>

---
+++ addons/source-python/plugins/udm/menus.py
@@ -0,0 +1,34 @@
+# ../udm/menus.py
+
+"""Provides a convenience class to add a 'close callback' to the PagedRadioMenu."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python Imports
+#   Menus
+from menus.radio import BUTTON_CLOSE
+from menus.radio import PagedRadioMenu
+
+
+# =============================================================================
+# >> PUBLIC CLASSES
+# =============================================================================
+class CloseButtonPagedMenu(PagedRadioMenu):
+    """Menu class used to react to the 'close' button."""
+
+    def __init__(self, close_callback, **kwargs):
+        """Object initialization."""
+        super().__init__(**kwargs)
+
+        # Store the close callback
+        self._close_callback = close_callback
+
+    def _select(self, player_index, choice_index):
+        """Override _select() to be able to react to the 'close' button within this menu."""
+        # Call the close callback if the 'close' button has been selected
+        if choice_index == BUTTON_CLOSE:
+            self._close_callback(player_index)
+
+        # Continue the base class routine
+        super()._select(player_index, choice_index)
