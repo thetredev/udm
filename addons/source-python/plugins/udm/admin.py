15f8b8b65e14eed08567ad5cdc4f75066254dc7b
BackRaw <backraw@gmx.de>
2017-11-07 04:18:07 +0100

    admin.py: Improve documentation
    Signed-off-by: BackRaw <backraw@gmx.de>

--- addons/source-python/plugins/udm/admin.py
+++ addons/source-python/plugins/udm/admin.py
@@ -19,16 +19,16 @@ from udm.spawnpoints.menus import SpawnPointManagerMenu
 
 
 # =============================================================================
-# >> PRIVATE CLASSES
+# >> ADMIN MENU
 # =============================================================================
 class _AdminMenu(CloseButtonPagedMenu):
-    """Class used to provide a base menu for each of the following submenus:
+    """Class used to provide a base menu with the following submenus:
 
-        - Spawn Point Manager
+        * Spawn Point Manager
     """
 
     def __init__(self):
-        """Object initialization"""
+        """Object initialization."""
         super().__init__(
             close_callback=self._close_callback,
             select_callback=lambda menu, player_index, option: option.value.send(player_index),
@@ -39,15 +39,18 @@ class _AdminMenu(CloseButtonPagedMenu):
 
     def _close_callback(self, player_index):
         """Enable default gameplay for the admin player who just closed this menu."""
+        # Get a PlayerEntity instance for the player
         player = PlayerEntity(player_index)
 
+        # Equip the player with their inventory selection
         player.equip(inventory_index=player.inventory_selection)
+
+        # Disable damage protection
         player.unprotect()
+
+        # Give knife
         player.give_named_item('weapon_knife')
 
 
-# =============================================================================
-# >> PUBLIC GLOBAL VARIABLES
-# =============================================================================
-# Create the Admin menu
+# Store a global instance of `_AdminMenu`
 admin_menu = _AdminMenu()
