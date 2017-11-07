acd8efd67a25092f24c766a39b2c628a3c0f9f5e
BackRaw <backraw@gmx.de>
2017-11-07 03:23:41 +0100

    players/__init__.py: Reorganize the file & improve documentation
    Signed-off-by: BackRaw <backraw@gmx.de>

--- addons/source-python/plugins/udm/players/__init__.py
+++ addons/source-python/plugins/udm/players/__init__.py
@@ -1,6 +1,6 @@
 # ../udm/players/__init__.py
 
-"""Provides a convenience class for player entities."""
+"""Provides an interface between a player entity and their inventories."""
 
 # =============================================================================
 # >> IMPORTS
@@ -43,39 +43,47 @@ from udm.weapons import weapons
 # >> PLAYER ENTITY
 # =============================================================================
 class PlayerEntity(Player):
-    """Convenience class used for the following things:
+    """Class used to provide the following functionality:
 
-        - Wrap players.entity.Player.give_named_item() to return an actual weapons.entity.Weapon instance
-        - Provide a safe and easy way to access the player's inventory"""
+        * inventories and inventory selections
+        * battle preparation including damage protection
+        * ammo refill
+    """
 
     def equip(self, inventory_index):
-        """Equip the player with their inventory."""
-        # Equip the player their inventory
+        """Equip the inventory at `inventory_index`."""
+        # Add a new inventory at `inventory_index` if none is present
         if inventory_index not in self.inventories:
             self.inventories[inventory_index] = PlayerInventory(self.uniqueid)
 
+        # Get the inventory at `inventory_index`
         inventory = self.inventories[inventory_index]
 
-        # Equip only if the player has chosen any weapons
+        # Equip all weapons in `inventory`
         if inventory:
             for item in inventory.values():
                 item.equip(self)
 
-        # Or give random weapons, if the inventory is empty
+        # Give random weapons, if `inventory` is empty
         else:
             for tag in weapons.tags:
                 self.give_named_item(random.choice(list(weapons.by_tag(tag))).name)
 
+    def strip(self, keep=None):
+        """Remove all the player's weapons except for those in `keep`."""
+        for weapon in self.weapons(not_filters=keep):
+            weapon.remove()
+
     def prepare(self):
         """Prepare the player for battle."""
-        # Equip the player with armor
+        # Give armor
         self.give_named_item('item_assaultsuit')
 
-        # Equip the player with a High Explosive grenade if configured that way
+        # Give a High Explosive grenade if configured that way
         if cvar_equip_hegrenade.get_int() > 0:
             self.give_named_item('weapon_hegrenade')
 
-        # Protect the player from any damage
+        # Enable damage protect
         self.protect(cvar_spawn_protection_delay.get_int())
 
         # Choose a random spawn point
@@ -86,40 +94,53 @@ class PlayerEntity(Player):
             self.origin = spawnpoint
             self.view_angle = spawnpoint.angle
 
-        # Strip the player off their weapons, but keep the knife
+        # Strip the player off their weapons, but keep melee weapons and grenades
         self.strip(('melee', 'grenade'))
 
-        # Equip the player
+        # Equip the current inventory
         self.equip(self.inventory_selection)
 
     def protect(self, time_delay=None):
-        """Protect the player for the configured spawn protection duration."""
+        """Enable damage protection and disable it after `time_delay` if `time_delay` is not None."""
         # Enable god mode
         self.godmode = True
 
-        # Set the player's color
+        # Set protection color
         self.color = Color(
             210 if self.team == 2 else 0,
             0,
             210 if self.team == 3 else 0
         )
 
-        # Call _unprotect() after the configured spawn protection duration
+        # Disable protection after `time_delay`
         if time_delay is not None:
             delay_manager[f'protect_{self.userid}'].append(Delay(time_delay, self.unprotect))
 
+    def unprotect(self):
+        """Disable damage protection."""
+        # Disable god mode
+        self.godmode = False
+
+        # Reset the color
+        self.color = WHITE
+
     def refill_ammo(self):
-        """Refill the player's ammo on reload after the reload animation has finished."""
+        """Refill the player's active weapon's ammo after the reload animation has finished."""
         with contextlib.suppress(ValueError):
-            if weapons[self.active_weapon.classname].tag != 'meele':
 
-                # Get the 'next attack' property for the current weapon, plus a tolerance value of one second
-                next_attack = self.active_weapon.get_property_float('m_flNextPrimaryAttack') + 1
+            # Only refill for non-melee and non-grenade weapons
+            if weapons[self.active_weapon.classname].tag not in ('melee', 'grenade'):
 
-                # Calculate the amount of time it would take for the reload animation to finish (tolerance included)
+                # Get the 'next attack' property for the current weapon
+                next_attack = self.active_weapon.get_property_float('m_flNextPrimaryAttack')
+
+                # Add a tolerance value of 1 second to somewhat counter the effects of lags, etc
+                next_attack += 1
+
+                # Calculate the amount of time it would take for the reload animation to finish
                 duration = next_attack - global_vars.current_time
 
-                # Refill the weapon's ammo after the reload animation has finished
+                # Call weapons.refill_ammo() after `duration`
                 delay_manager[f'refill_{self.active_weapon.index}'].append(
                     Delay(duration, refill_ammo, (self.active_weapon, ))
                 )
@@ -129,24 +150,9 @@ class PlayerEntity(Player):
         if self.is_connected():
             super().spawn(True)
 
-    def strip(self, keep=None):
-        """Remove all the player's weapons except for 'knife'."""
-        for weapon in self.weapons(not_filters=keep):
-            weapon.remove()
-
-    def unprotect(self):
-        """Enable default gameplay, if they're still online."""
-        if self.is_connected():
-
-            # Disable god mode
-            self.godmode = False
-
-            # Reset the player's color
-            self.color = WHITE
-
     @property
     def inventories(self):
-        """Provide access to the player's inventories."""
+        """Return the player's inventories."""
         return player_inventories[self.uniqueid]
 
     def set_inventory_selection(self, inventory_index):
@@ -154,12 +160,13 @@ class PlayerEntity(Player):
         player_inventories.selections[self.userid] = inventory_index
 
     def get_inventory_selection(self):
-        """Return the player's inventory selection - 0 if not present."""
+        """Return the player's current inventory selection - 0 if not present."""
         if self.userid in player_inventories.selections:
             return player_inventories.selections[self.userid]
 
         return 0
 
+    # Set the `inventory_selection` property for PlayerEntity
     inventory_selection = property(get_inventory_selection, set_inventory_selection)
 
 
