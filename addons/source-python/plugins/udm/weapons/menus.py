1c4d40915fa1a0734fe752d4cdc87b38ab57e573
BackRaw <backraw@gmx.de>
2017-11-07 02:02:17 +0100

    weapons.menus._WeaponMenu: Add close callback
    This allows players to choose only one weapon for an inventory.
    
    Signed-off-by: BackRaw <backraw@gmx.de>

--- addons/source-python/plugins/udm/weapons/menus.py
+++ addons/source-python/plugins/udm/weapons/menus.py
@@ -7,10 +7,11 @@
 # =============================================================================
 # Source.Python Imports
 #   Menus
-from menus.radio import PagedRadioMenu
 from menus.radio import PagedRadioOption
 
 # Script Imports
+#   Menus
+from udm.menus import CloseButtonPagedMenu
 #   Players
 from udm.players import PlayerEntity
 #   Weapons
@@ -20,7 +21,7 @@ from udm.weapons import weapons
 # =============================================================================
 # >> PRIVATE CLASSES
 # =============================================================================
-class _WeaponMenu(PagedRadioMenu):
+class _WeaponMenu(CloseButtonPagedMenu):
     """Convenience class used to create weapon menus dynamically on-the-fly."""
 
     def __init__(self, tag, title, next_menu):
@@ -28,12 +29,19 @@ class _WeaponMenu(PagedRadioMenu):
         # Call the super class constructor using the list of weapons mapped to the tag
         # and this menu's select callback
         super().__init__(
-            [PagedRadioOption(weapon.display_name, weapon.name) for weapon in weapons.by_tag(tag)],
-            select_callback=self.select_callback, title=title
+            close_callback=self._close_callback,
+            data=[PagedRadioOption(weapon.display_name, weapon.name) for weapon in weapons.by_tag(tag)],
+            select_callback=self.select_callback,
+            title=title
         )
 
         # Store the next menu
         self._next_menu = next_menu
+
+    def _close_callback(self, player_index):
+        """Send the next menu if the player closes this one."""
+        if self._next_menu is not None:
+            self._next_menu.send(player_index)
 
     def select_callback(self, menu, player_index, option):
         """Handle the chosen menu item."""
