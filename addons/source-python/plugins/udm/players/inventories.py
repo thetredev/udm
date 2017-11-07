7681aea73ae3a3ee3cce54b40cf8f2138e729556
BackRaw <backraw@gmx.de>
2017-11-07 03:31:53 +0100

    players/inventories.py: Improve documentation
    Signed-off-by: BackRaw <backraw@gmx.de>

--- addons/source-python/plugins/udm/players/inventories.py
+++ addons/source-python/plugins/udm/players/inventories.py
@@ -1,6 +1,6 @@
 # ../udm/players/inventories.py
 
-"""Provides convenience classes for player inventories."""
+"""Provides player inventories."""
 
 # =============================================================================
 # >> IMPORTS
@@ -16,42 +16,41 @@ from udm.weapons import weapons
 
 
 # =============================================================================
-# >> PRIVATE CLASSES
+# >> INVENTORY ITEM
 # =============================================================================
 class _InventoryItem(object):
     """Class used to provide an inventory item."""
 
     def __init__(self, classname):
-        """Object initialization."""
-        # Store the item's classname
+        """Store the item's classname."""
         self.classname = classname
 
     def equip(self, player):
         """Equip the player with this inventory item."""
-        # Get a list of weapons of the same tag as this item owned by the player
+        # Get a list of weapons owned by the player of the same weapon tag as this inventory item
         equipped = list(player.weapons(is_filters=self.tag))
 
         # Remove the weapon the player currently owns before equipping this one
         if equipped:
             entity = equipped[0]
 
-            # Don't equip if this item already is the weapon owned by the player
+            # Only equip if this item is not the same the player currently owns
             if entity.classname != self.classname:
                 entity.remove()
                 player.give_named_item(self.classname)
 
-        # Equip the item straight away, if the player doesn't own any weapon of its tag
+        # Equip the item straight away, if the player doesn't own any weapon of this inventory item's weapon tag
         else:
             player.give_named_item(self.classname)
 
     @property
     def tag(self):
-        """Return this item's weapon tag."""
+        """Return this inventory item's weapon tag."""
         return weapons[self.classname].tag
 
 
 # =============================================================================
-# >> PUBLIC CLASSES
+# >> PLAYER INVENTORIES
 # =============================================================================
 class PlayerInventory(dict):
     """Class used to provide a weapon inventory for players."""
@@ -61,47 +60,45 @@ class PlayerInventory(dict):
         # Call the super class constructor
         super().__init__()
 
-        # Store the player's index
+        # Store the player's SteamID
         self._player_steamid = player_steamid
 
     def add_weapon(self, classname):
-        """Add the weapon to the inventory and equip the player with it."""
+        """Add an inventory item for `classname` and equip the player with it."""
         # Get the weapon's class
         weapon_class = weapons[classname]
 
-        # Create a new inventory item if the player doesn't own any weapon of the same tag
+        # Create a new inventory item if the player doesn't own any weapon of the weapon's tag
         if weapon_class.tag not in self.keys():
             self[weapon_class.tag] = _InventoryItem(classname)
 
-        # Get the inventory item object
+        # Get the inventory item
         item = self[weapon_class.tag]
 
-        # Set the item's classname
+        # Set the inventory item's classname
         item.classname = classname
 
-        # Equip the player with the item
+        # Equip the player with it
         player = Player(index_from_steamid(self._player_steamid))
         item.equip(player)
 
     def values(self):
+        """Return all inventory items reverse-sorted by their weapon tag."""
         for key in sorted(self.keys(), reverse=True):
             yield self[key]
 
 
 class _PlayerInventories(dict):
-    """Class used to store player inventories and their weapon selections."""
+    """Class used to provide multiple inventories and weapon selections for players."""
 
-    # Store each player's weapon selections
+    # Store player weapon selections
     selections = dict()
 
     def __missing__(self, key):
-        """Set and return an empty dictionary object as the value for `key`."""
+        """Set and return an empty dictionary as the value for `key`."""
         value = self[key] = dict()
         return value
 
 
-# =============================================================================
-# >> PUBLIC GLOBAL VARIABLES
-# =============================================================================
-# Create a global instance of _PlayerInventories
+# Store a global instance of `_PlayerInventories`
 player_inventories = _PlayerInventories()
