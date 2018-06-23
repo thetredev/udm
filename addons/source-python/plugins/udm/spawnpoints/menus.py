b101d0ba657cd499d4b76c7dced6158bdcb872dd
BackRaw <backraw@gmx.de>
2018-06-23 23:45:24 +0200

    spawnpoints.menus: Get rid of the conditioning inside on_spawnpoints_manager_menu_select()
    This is done by moving each block of code associated to a menu option
    into their own functions and assigning the respective callback as the
    menu option itself.
    
    Signed-off-by: BackRaw <backraw@gmx.de>

--- addons/source-python/plugins/udm/spawnpoints/menus.py
+++ addons/source-python/plugins/udm/spawnpoints/menus.py
@@ -34,22 +34,75 @@ SPAWN_POINT_TOLERANCE_UNITS = 20.0
 # =============================================================================
 # >> SPAWN POINTS MANAGER MENU
 # =============================================================================
-# Store menu options
-SPAWNPOINTS_MANAGER_ADD = 0
-SPAWNPOINTS_MANAGER_REMOVE = 1
-SPAWNPOINTS_MANAGER_LIST = 2
-SPAWNPOINTS_MANAGER_SAVE = 3
+def add_spawnpoint_at_player_location(player):
+    """Add a the player's current location as a spawn point."""
+    # Get a list of the distance of all spawn points to the player's current location
+    distances = [spawnpoint.get_distance(player.origin) for spawnpoint in spawnpoint_manager]
+
+    # Add the player's current location, if it is far enough away from all other spawn points
+    if not distances or min(distances) >= SAFE_SPAWN_DISTANCE:
+        spawnpoint_manager.append(SpawnPoint(player.origin.x, player.origin.y, player.origin.z, player.view_angle))
+
+        # Tell the player about the addition
+        player.tell(
+            spawnpoints_manager_menu.title,
+            f'Spawn Point {MESSAGE_COLOR_WHITE}#{len(spawnpoint_manager)} {MESSAGE_COLOR_ORANGE}has been added.'
+        )
+
+    # Send the spawn points manager menu back to the player
+    spawnpoints_manager_menu.send(player.index)
+
+
+def remove_spawnpoint_at_player_location(player):
+    """Remove the spawn point at the player's current location."""
+    # Find the spawn point closest to the player's current location
+    for spawnpoint in spawnpoint_manager.copy():
+        if spawnpoint in spawnpoint_manager and spawnpoint.get_distance(player.origin) < SPAWN_POINT_TOLERANCE_UNITS:
+
+            # Store its position
+            position = spawnpoint_manager.index(spawnpoint) + 1
+
+            # Remove it from the spawn points list
+            spawnpoint_manager.remove(spawnpoint)
+
+            # Tell the player about the removal
+            player.tell(
+                spawnpoints_manager_menu.title,
+                f'Spawn Point {MESSAGE_COLOR_WHITE}#{position} {MESSAGE_COLOR_ORANGE}has been removed.'
+            )
+
+            # Break the loop
+            break
+
+    # Send the spawn points manager menu back to the player
+    spawnpoints_manager_menu.send(player.index)
+
+
+def send_spawnpoints_list_to_player(player):
+    """Send the spawn points list menu to the player."""
+    _spawnpoints_list_menu.send(player.index)
+
+
+def save_spawnpoints(player):
+    """Save current spawn points to file."""
+    spawnpoint_manager.save()
+
+    # Tell the player about it
+    player.tell(spawnpoints_manager_menu.title, 'Spawn Points have been saved.')
+
+    # Send the spawn points manager menu back to the player
+    spawnpoints_manager_menu.send(player.index)
 
 
 # Create the Spawn Points Manager menu
 spawnpoints_manager_menu = PagedMenu(
     [
-        PagedOption('Add', SPAWNPOINTS_MANAGER_ADD),
-        PagedOption('Remove', SPAWNPOINTS_MANAGER_REMOVE),
+        PagedOption('Add', add_spawnpoint_at_player_location),
+        PagedOption('Remove', remove_spawnpoint_at_player_location),
         ' ',
-        PagedOption('List', SPAWNPOINTS_MANAGER_LIST),
+        PagedOption('List', send_spawnpoints_list_to_player),
         ' ',
-        PagedOption('Save', SPAWNPOINTS_MANAGER_SAVE)
+        PagedOption('Save', save_spawnpoints)
     ], title='Spawn Points Manager'
 )
 
@@ -68,9 +121,7 @@ _spawnpoints_list_menu = PagedMenu(title='Spawn Points List')
 def on_spawnpoints_list_menu_build(menu, player_index):
     """Reload the menu with all available spawn points."""
     menu.clear()
-    menu.extend(
-        [PagedOption(f'#{index + 1}', spawnpoint) for index, spawnpoint in enumerate(spawnpoint_manager)]
-    )
+    menu.extend([PagedOption(f'#{index + 1}', spawnpoint) for index, spawnpoint in enumerate(spawnpoint_manager)])
 
 
 @_spawnpoints_list_menu.register_select_callback
@@ -102,66 +153,10 @@ def on_spawnpoints_manager_menu_select(menu, player_index, option):
     # Get a PlayerEntity instance for the player
     player = PlayerEntity(player_index)
 
-    # Handle the option `Add`
-    if option.value == SPAWNPOINTS_MANAGER_ADD:
+    # Call the callback function from `option` on `player`
+    option.value(player)
 
-        # Get a list of the distance of all spawn points to the player's current location
-        distances = [spawnpoint.get_distance(player.origin) for spawnpoint in spawnpoint_manager]
 
-        # Add the player's current location, if it is far enough away from all other spawn points
-        if not distances or min(distances) >= SAFE_SPAWN_DISTANCE:
-            spawnpoint_manager.append(SpawnPoint(player.origin.x, player.origin.y, player.origin.z, player.view_angle))
-
-            # Tell the player about the addition
-            player.tell(
-                spawnpoints_manager_menu.title,
-                f'Spawn Point {MESSAGE_COLOR_WHITE}#{len(spawnpoint_manager)} {MESSAGE_COLOR_ORANGE}has been added.'
-            )
-
-        # Send this menu back to the player
-        spawnpoints_manager_menu.send(player.index)
-
-    # Handle the option `Remove`
-    elif option.value == SPAWNPOINTS_MANAGER_REMOVE:
-
-        # Find the spawn point closest to the player's current location
-        for spawnpoint in spawnpoint_manager.copy():
-            if spawnpoint in spawnpoint_manager and spawnpoint.get_distance(player.origin) < SPAWN_POINT_TOLERANCE_UNITS:
-                # Store its position
-                position = spawnpoint_manager.index(spawnpoint) + 1
-
-                # Remove it from the spawn points list
-                spawnpoint_manager.remove(spawnpoint)
-
-                # Tell the player about the removal
-                player.tell(
-                    spawnpoints_manager_menu.title,
-                    f'Spawn Point {MESSAGE_COLOR_WHITE}#{position} {MESSAGE_COLOR_ORANGE}has been removed.'
-                )
-
-                # Break the loop
-                break
-
-        # Send this menu back to the player
-        spawnpoints_manager_menu.send(player.index)
-
-    # For `List`: Send the _SpawnPointManagerListMenu to the player
-    elif option.value == SPAWNPOINTS_MANAGER_LIST:
-        _spawnpoints_list_menu.send(player.index)
-
-    # Handle the option `Save`
-    elif option.value == SPAWNPOINTS_MANAGER_SAVE:
-
-        # Save the spawn points list to file
-        spawnpoint_manager.save()
-
-        # Tell the player about it
-        player.tell(
-            spawnpoints_manager_menu.title, 'Spawn Points have been saved.'
-        )
-
-        # Send this menu back to the player
-        spawnpoints_manager_menu.send(player.index)
 @spawnpoints_manager_menu.register_close_callback
 def on_spawn_points_manager_menu_close(menu, player_index):
     """Send the Admin Menu when the player closes the Spawn Points Manager menu."""
