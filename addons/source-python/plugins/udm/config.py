faa2bc2473c154bbae27d0b2494e571a3d4cdd4d
BackRaw <backraw@gmx.de>
2018-06-23 21:49:28 +0200

    config: Remove redundant comments above cvar declarations
    Signed-off-by: BackRaw <backraw@gmx.de>

--- addons/source-python/plugins/udm/config.py
+++ addons/source-python/plugins/udm/config.py
@@ -24,28 +24,23 @@ with ConfigManager(info.name, f'{info.name}_') as config:
     config.text('   * General')
     config.text('----------------------------------')
 
-    # Enable infinite ammo?
     cvar_enable_infinite_ammo = config.cvar(
         'enable_infinite_ammo', 1, 'Enable infinite ammo?'
     )
 
-    # The respawn delay in seconds
     cvar_respawn_delay = config.cvar(
         'respawn_delay', 2, 'The respawn delay in seconds.'
     )
 
-    # Enable or disable non-blocking mode for players
     cvar_enable_noblock = config.cvar(
         'enable_noblock', 1,
         'Enable or disable non-blocking mode for players.'
     )
 
-    # The spawn protection duration in seconds
     cvar_spawn_protection_delay = config.cvar(
         'spawn_protection_delay', 2, 'The spawn protection delay in seconds.'
     )
 
-    # The minimum distance players have to have between a spawn point for it to be 'safe'
     cvar_spawn_point_distance = config.cvar(
         'spawn_point_distance', 150,
         "The minimum distance players have to have between a spawn point for it to be 'safe'."
@@ -55,13 +50,11 @@ with ConfigManager(info.name, f'{info.name}_') as config:
     config.text('   * Kill Rewards')
     config.text('----------------------------------')
 
-    # Refill the killer's clip if they killed an enemy with a headshot
     cvar_refill_clip_on_headshot = config.cvar(
         'refill_clip_on_headshot', 1,
         "Refill the killer's clip if they killed an enemy with a headshot."
     )
 
-    # Restore the killer's health to 100HP if they killed an enemy with the knife
     cvar_restore_health_on_knife_kill = config.cvar(
         'restore_health_on_knife_kill', 1,
         "Restore the killer's health to 100HP if they killed an enemy with the knife."
@@ -71,12 +64,10 @@ with ConfigManager(info.name, f'{info.name}_') as config:
     config.text('   * High Explosive Grenade')
     config.text('----------------------------------')
 
-    # High Explosive grenade equipment behaviour
     cvar_equip_hegrenade = config.cvar(
         'equip_hegrenade', 2, 'High Explosive grenade behaviour'
     )
 
-    # Options for cvar_equip_hegrenade
     cvar_equip_hegrenade.Options.append('0 = Off')
     cvar_equip_hegrenade.Options.append('1 = Equip on spawn')
     cvar_equip_hegrenade.Options.append('2 = Equip on spawn and on each HE grenade kill')
@@ -86,14 +77,11 @@ with ConfigManager(info.name, f'{info.name}_') as config:
     config.text('   * Team Changes Per Round')
     config.text('----------------------------------')
 
-    # The maximum amount of times a players are allowed to change their team per round.
     cvar_team_changes_per_round = config.cvar(
         'team_changes_per_round', 2,
         'The maximum amount of times a players are allowed to change their team per round.'
     )
 
-    # The time in minutes a player who exceeded the maximum team change count has to wait until they can choose a
-    # team again
     cvar_team_changes_reset_delay = config.cvar(
         'team_changes_reset_delay', 1.5,
         'The time in minutes a player who exceeded the maximum team change count has to wait until they can choose a'
@@ -104,12 +92,10 @@ with ConfigManager(info.name, f'{info.name}_') as config:
     config.text('   * Chat Commands')
     config.text('----------------------------------')
 
-    # The chat command used to open the admin menu
     cvar_saycommand_admin = config.cvar(
         'saycommand_admin', '!udm', 'The chat command used to open the admin menu.'
     )
 
-    # The chat command used to open the weapons menu
     cvar_saycommand_guns = config.cvar(
         'saycommand_guns', 'guns', 'The chat command used to open the weapons menu.'
     )
