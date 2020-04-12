4284d61753e9651d1fdeb6233ff9a410e9045d2b
Timo Reichl <timo.reichl@mailbox.org>
2020-04-12 05:08:19 +0200

    cvars: Handle gamemode_deathmatch cvars
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- addons/source-python/plugins/udm/cvars.py
+++ addons/source-python/plugins/udm/cvars.py
@@ -14,6 +14,8 @@ from cvars import cvar
 # Script Imports
 #   Config
 from udm.config import cvar_enable_noblock
+from udm.config import cvar_enable_infinite_ammo
+from udm.config import cvar_spawn_protection_delay
 
 
 # =============================================================================
@@ -27,26 +29,14 @@ class DefaultConVar(object):
         # Store the convar
         self.convar = cvar.find_var(name)
 
-        # Get its default value, if it exists
-        if self.convar is not None:
-
-            if isinstance(value, int):
-                value = self.convar.get_int()
-
-            elif isinstance(value, str):
-                value = self.convar.get_string()
-
-            elif isinstance(value, bool):
-                value = self.convar.get_bool()
-
-            else:
-                raise NotImplementedError(f'Type {type(value)} is not supported for this class.')
-
-        # Store its default value
-        self.default = value
-
         # Store its runtime value
         self.value = value
+
+        # Store its default value, if it exists
+        if self.convar is not None:
+            self.default = self.convar.get_int()
+        else:
+            self.default = None
 
     def manipulate_value(self):
         """Set the manipulated value."""
@@ -55,7 +45,7 @@ class DefaultConVar(object):
 
     def set_default_value(self):
         """Set the default value."""
-        if self.convar is not None:
+        if self.convar is not None and self.default is not None:
             self.convar.set_int(self.default)
 
 
@@ -83,7 +73,19 @@ default_convars = DefaultConVars([
     DefaultConVar('mp_buytime', 60 * 60),
     DefaultConVar('mp_startmoney', 10_000),
     DefaultConVar('mp_buy_anywhere', 1),
-    DefaultConVar('mp_solid_teammates', int(cvar_enable_noblock.get_int() == 0))
+    DefaultConVar('mp_solid_teammates', int(cvar_enable_noblock.get_int() == 0)),
+    DefaultConVar('mp_respawn_on_death_t', 0),
+    DefaultConVar('mp_respawn_on_death_ct', 0),
+    DefaultConVar('mp_buy_during_immunity', 0),
+    DefaultConVar('mp_respawn_immunitytime', 0 if cvar_spawn_protection_delay.get_int() > 0 else 2),
+    DefaultConVar('sv_infinite_ammo', 0 if cvar_enable_infinite_ammo.get_bool() else 2),
+    DefaultConVar('mp_friendlyfire', 0),
+    DefaultConVar('mp_freezetime', 0),
+    DefaultConVar('mp_dm_bonus_length_max', 0),
+    DefaultConVar('mp_dm_bonus_length_min', 0),
+    DefaultConVar('mp_dm_time_between_bonus_max', 9999),
+    DefaultConVar('mp_dm_time_between_bonus_min', 9999),
+    DefaultConVar('mp_do_warmup_period', 0)
 ])
 
 # Store the convar `mp_restartgame`
